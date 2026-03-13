a08d9705763c66b80d6be0d4751e779eab4fb544
Sidra Ghaffar <sidraghaffar04@gmail.com>
2026-03-13 23:07:51 +0500

    constructor_decorators.py

--- constructor_decorators.py
+++ constructor_decorators.py
@@ -180,26 +180,167 @@
 # obj.show()
 
 
-def decorate(end):
-    def first(self,a,b):
-        print(f"Method {end.__name__} is running")
-        end(self,a,b)
-        print(f"Method {end.__name__} is running")
+# def decorate(end):
+#     def first(self,a,b):
+#         print(f"Method {end.__name__} is running")
+#         end(self,a,b)
+#         print(f"Method {end.__name__} is running")
+#     return first
+
+# class Calculator:
+#     @decorate
+#     def Add(self,a,b):
+#         sum=a+b
+#         print("Sum:",sum)
+#     @decorate
+#     def Mullti(self,a,b):
+#         multi=a*b
+#         print("Multiply:",multi)
+
+# a=Calculator()
+# a.Add(65,1)
+# a.Mullti(5,5)
+
+
+# class Calculator:
+#     @decorate
+#     def Add(self,a,b):
+#         sum=a+b
+#         print("Sum:",sum)
+#     @decorate
+#     def Mullti(self,a,b):
+#         multi=a*b
+#         print("Multiply:",multi)
+
+# a=Calculator()
+# a.Add(65,1)
+# a.Mullti(5,5)
+
+# def decorate(end):
+#     def first(self,*args,**kwargs):
+#         print("Good Morning")
+#         end(self,*args,**kwargs)
+#         print("Good Bye")
+#     return first
+
+# class Teacher:
+#     def __init__(self,name=None,subject=None):
+#         self.name=name
+#         self.subject=subject
+#     @decorate
+#     def show(self):
+#         print("Name:",self.name)
+#         print("Subject:",self.subject)
+
+# obj=Teacher()
+# obj1=Teacher("Rana Nadeem","Programming")
+# obj.show()
+# obj1.show()
+
+
+# # Example: Understanding Python Decorators
+
+# def decorator(func):
+#     def wrapper(*args, **kwargs):
+#         print("Function execution started...")
+        
+#         # Calling the original function
+#         func(*args, **kwargs)
+        
+#         print("Function execution finished...")
+#     return wrapper
+
+
+# class Calculator:
+
+#     @decorator
+#     def add(self, a, b):
+#         result = a + b
+#         print("Addition Result:", result)
+
+#     @decorator
+#     def multiply(self, a, b):
+#         result = a * b
+#         print("Multiplication Result:", result)
+
+
+# # Creating object
+# calc = Calculator()
+
+# # Calling methods
+# calc.add(10, 5)
+# calc.multiply(4, 6)
+
+
+
+
+
+# #Decorators
+# def decorators(end):
+#     def first(self):
+#         print("Function execution started")
+#         end(self)
+#         print("Function executuion funished")
+#     return first
+
+# #CLass
+# class Add:
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#         #At this ponit we use decorators
+#     @decorators
+#     def show(self):
+#         print("Sum:",self.a+self.b)
+
+# #Object make
+# obj=Add(3,8)
+# obj.show()
+
+
+
+import time
+def decorators(end):
+    def first(*args,**kwargs):
+        print(f"function {end.__name__} is used")
+        end(*args,**kwargs)
+        print(f"Function {end.__name__} is funished")
     return first
 
+def timedeco(end):
+    def first(*args,**kwargs):
+        start=time.time()
+        end(*args,**kwargs)
+        finsh=time.time()
+        print(f"Execution time is {finsh-start} seconds")
+    return first
 class Calculator:
-    @decorate
-    def Add(self,a,b):
-        sum=a+b
-        print("Sum:",sum)
-    @decorate
-    def Mullti(self,a,b):
-        multi=a*b
-        print("Multiply:",multi)
+    @timedeco
+    @decorators
+    def add(self,a,b):
+        print("Sum:",a+b)
 
-a=Calculator()
-a.Add(65,1)
-a.Mullti(5,5)
+    @timedeco
+    @decorators
+    def subtract(self,a,b):
+        print("Subtraction:",a-b)
+    @timedeco
+    @decorators
+    def multiply(self,a,b):
+        print("Multiplication:",a*b)
+    
+    @timedeco
+    @decorators
+    def divide(self,a,b):
+        print("Division:",a/b)
+
+obj=Calculator()
+obj.add(8,6)
+# obj.subtract(8,6)
+# obj.multiply(8,6)
+# obj.divide(8,6)
+
+
 
 
 
