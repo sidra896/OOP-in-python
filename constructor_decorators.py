4d24de1351238b66eee1f881b9189563a87e6845
Sidra Ghaffar <sidraghaffar04@gmail.com>
2026-03-14 19:58:54 +0500

    Update constructor_decorators.py

--- constructor_decorators.py
+++ constructor_decorators.py
@@ -299,46 +299,235 @@
 
 
 
-import time
-def decorators(end):
-    def first(*args,**kwargs):
-        print(f"function {end.__name__} is used")
-        end(*args,**kwargs)
-        print(f"Function {end.__name__} is funished")
-    return first
+# import time
+# def decorators(end):
+#     def first(*args,**kwargs):
+#         print(f"function {end.__name__} is used")
+#         end(*args,**kwargs)
+#         print(f"Function {end.__name__} is funished")
+#     return first
 
-def timedeco(end):
-    def first(*args,**kwargs):
-        start=time.time()
-        end(*args,**kwargs)
-        finsh=time.time()
-        print(f"Execution time is {finsh-start} seconds")
-    return first
-class Calculator:
-    @timedeco
-    @decorators
-    def add(self,a,b):
-        print("Sum:",a+b)
+# def timedeco(end):
+#     def first(*args,**kwargs):
+#         start=time.time()
+#         end(*args,**kwargs)
+#         finsh=time.time()
+#         print(f"Execution time is {finsh-start} seconds")
+#     return first
+# class Calculator:
+#     @timedeco
+#     @decorators
+#     def add(self,a,b):
+#         print("Sum:",a+b)
 
-    @timedeco
-    @decorators
-    def subtract(self,a,b):
-        print("Subtraction:",a-b)
-    @timedeco
-    @decorators
-    def multiply(self,a,b):
-        print("Multiplication:",a*b)
+#     @timedeco
+#     @decorators
+#     def subtract(self,a,b):
+#         print("Subtraction:",a-b)
+#     @timedeco
+#     @decorators
+#     def multiply(self,a,b):
+#         print("Multiplication:",a*b)
     
-    @timedeco
-    @decorators
-    def divide(self,a,b):
-        print("Division:",a/b)
+#     @timedeco
+#     @decorators
+#     def divide(self,a,b):
+#         print("Division:",a/b)
 
-obj=Calculator()
-obj.add(8,6)
+# obj=Calculator()
+# obj.add(8,6)
 # obj.subtract(8,6)
 # obj.multiply(8,6)
 # obj.divide(8,6)
+
+# class Student:
+#     def __init__(self,name,m1,m2,m3):
+#         self.name=name
+#         self.m1=m1
+#         self.m2=m2
+#         self.m3=m3
+
+#     @property    
+#     def average(self):
+#         avr=self.m1+self.m2+self.m3
+#         print("Name:",self.name)
+#         print("Average Marks:",avr)
+        
+    
+# obj=Student("Sidra",65,90,54)
+# obj.average
+
+
+# class Calculator:
+#     @staticmethod
+#     def add(a,b):
+#         print("Addition:",a+b)
+
+#     @staticmethod
+#     def multi(a,b):
+#         print("Multiplication:",a*b)
+
+# obj=Calculator()
+# obj.add(8,8)
+# obj.multi(8,2)
+
+
+# class Student:
+#     first_name="Sidra"
+
+#     def __init__(self,name):
+#         self.name=name
+
+#     @classmethod
+#     def nick_first(cls,new_name):
+#         cls.first_name=new_name
+
+# obj=Student("Sidra")
+# print(Student.first_name)
+# Student.nick_first("Semi")
+# print(Student.first_name)
+
+
+# class Bank:
+#     bank_name="HBL Bank"
+#     def __init__(self,holder_name,balance):
+#         self.holder_name=holder_name
+#         self.balance=balance
+
+#     @property
+#     def info(self):
+#         print("Name of the bank:",Bank.bank_name)
+#         print("Holder Name:",self.holder_name)
+#         print("Balance in Account:",self.balance)
+#     @staticmethod
+#     def rules():
+#         print("Balance in bank account shold be more than 1000")
+
+#     @classmethod
+#     def change_bank_name(cls,new_name):
+#         cls.bank_name=new_name
+
+# obj=Bank("Sidra",50000000)
+# obj.info
+# obj.rules()
+# Bank.change_bank_name("National Bank")
+# print("Updated bank name:",Bank.bank_name)
+
+
+
+# class Rectangle:
+
+#     def __init__(self,length,width):
+#         self.length=length
+#         self.width=width
+
+#     @property
+#     def Area(self):
+#         print("Area:",self.length*self.width)
+
+#     @staticmethod
+#     def info():
+#         print("Rectangle has 4 Sides")
+
+
+
+# obj=Rectangle(3,4)
+# obj.Area
+# obj.info()
+
+
+# class Teacher:
+#     school_name="City School"
+
+#     def __init__(self,name,subject):
+#         self.name=name
+#         self.subject=subject
+    
+#     @property
+#     def show(self):
+#         print("School Name:",Teacher.school_name)
+#         print("Teacher Name:",self.name)
+#         print("Subject:",self.subject)
+
+#     @staticmethod
+#     def rules():
+#         print("Has to maintain the Rules in Lab")
+
+#     @classmethod
+#     def change_school_name(cls,new_name):
+#        cls. school_name=new_name
+
+# obj=Teacher("Rana Nadeem","programming")   
+# obj.show
+# obj.rules()
+# Teacher.change_school_name("Govt College Burewala")
+# print("School name updated:",Teacher.school_name)
+
+
+
+# class Employee:
+#     company_name="Technology Base Company"
+
+#     def __init__(self,name,salary,depart):
+#         self.name=name
+#         self.salary=salary
+#         self.depart=depart
+
+#     @property
+#     def details(self):
+#         print("Company Name:",Employee.company_name)
+#         print("Employee Name:",self.name)
+#         print("Employee Salary:",self.salary)
+#         print("Department of Employee:",self.depart)
+
+#     @staticmethod
+#     def rules():
+#         print("All Employees must submit weekly reports.")
+
+#     @classmethod
+#     def change_company_name(cls,new_name):
+#         cls.company_name=new_name
+
+
+
+# obj=Employee("Sidra",500000,"Office Managment")
+# obj.details
+# obj.rules()
+# Employee.change_company_name("Tech company")
+# print("New Company Name:",Employee.company_name)
+
+
+
+# class Library:
+#     library_name="City Library"
+
+#     def __init__(self,title,name,price):
+#         self.title=title
+#         self.name=name
+#         self.price=price
+
+#     @property
+#     def info(self):
+#         print("Library Name:",Library.library_name)
+#         print("Title of the Book:",self.title)
+#         print("Name of the Author:",self.name)
+#         print("Price Of the Book:",self.price)
+
+#     @staticmethod
+#     def rules():
+#         print("One the book is buy it is not return")
+
+#     @classmethod
+#     def change_library_name(cls,new_name):
+#         cls.library_name=new_name
+
+
+
+# obj=Library("Peer e Kamal","Umera Ahmed",5000)
+# obj.info
+# obj.rules()
+# Library.change_library_name("Public Library")
+# print("Updated Library Name:",Library.library_name)
 
 
 
